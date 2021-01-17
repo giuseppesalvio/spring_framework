f3a11939d9daa290a931446b2d413134303d9890
g.salvio <giuseppe.salvio@finconsgroup.com>
2021-01-17 23:09:32 +0100

    Revert "eliminato value non serve ancora qui"
    This reverts commit 362ce338

--- src/main/java/com/salvio/config/ApplicationConfig.java
+++ src/main/java/com/salvio/config/ApplicationConfig.java
@@ -11,6 +11,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfig {
 
+    @Value("Hello")
+    private String greeting;
+
     @Autowired
     private GreetingService greetingService;
     @Autowired
